8431cb336be640d8142ccdb44812e8a0e6265f45
bm1729 <ben.mumford1729@gmail.com>
2015-02-19 17:01:11 +0000

    Added basic ajax functionality

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./src/server/index');
+var data = require('./src/server/data');
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, 'src/client')));
 app.use(express.static(path.join(__dirname, 'node_modules')));
 
 app.use('/', routes);
+app.use('/data', data);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- src/client/javascripts/controller/main.controller.js
+++ src/client/javascripts/controller/main.controller.js
@@ -2,6 +2,11 @@
 
 var appModule = angular.module('app', []);
 
-appModule.controller('mainController', function($scope) {
+appModule.controller('mainController', function($scope, $http) {
     $scope.message = 'Hello world';
+    $scope.getMessage = function() {
+        $http.get('data').success(function(data) {
+            $scope.message = data.message;
+        });
+    };
 });
