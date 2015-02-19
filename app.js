6552ed66b47674cc200388a407195a8de1b4ae0b
bm1729 <ben.mumford1729@gmail.com>
2015-02-19 15:19:37 +0000

    More shuffling of the express application

--- app.js
+++ app.js
@@ -6,12 +6,11 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var routes = require('./src/routes/index');
-var users = require('./src/routes/users');
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, 'src/public/views'));
 app.set('view engine', 'jade');
 
 // uncomment after placing your favicon in /public
@@ -23,7 +22,6 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'src/public')));
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
